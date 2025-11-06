169c3b22f05c1c9934412f727cf120c0281b5480
DylanSatow <dylansatow531@gmail.com>
2025-11-05 23:05:31 -0500

    added projects

--- src/lib/components/AboutSection.svelte
+++ src/lib/components/AboutSection.svelte
@@ -5,14 +5,14 @@
 <div class="space-y-4">
 	<div>
 		<span class="text-muted-foreground">$</span>
-		<span class="text-foreground ml-2">cat aboutMe.txt</span>
+		<span class="text-foreground ml-2">cat about_me.txt</span>
 	</div>
 	<p class="leading-relaxed text-foreground">
 		I'm Dylan, a senior at Columbia University. I'm super into broadening my horizons in both math and computer science, with my current interests being Mechanistic Interpretability and Representation Learning. I also love more general CS/Software Engineering (shoutout NixOS), and have recently gotten much more into high performance computing. I am also an avid jazz trumpet player/multi-instrumentalist, with experience playing at many notable venues in NYC
 	</p>
 	<div class="mt-6">
 		<span class="text-muted-foreground">$</span>
-		<span class="text-foreground ml-2">cat skills.txt</span>
+		<span class="text-foreground ml-2">cat what_im_using_right_now.txt</span>
 	</div>
 	<div class="flex flex-wrap gap-2 mt-2">
 		{#each skills as skill}
