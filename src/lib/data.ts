a282a0050c6e95588f340e825b3af6d94d065de4
DylanSatow <dylansatow531@gmail.com>
2025-11-06 14:48:42 -0500

    changed research

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -9,7 +9,7 @@ export const research = [
 	{ role: 'Non-Euclidean Representation Learning', company: "Pe'er Lab @ Columbia", period: 'Jan 2025 - Aug 2025', description: 'Coauthor/contributor to non-euclidean representation learning library w/ 60+ stars on github. Implemented all curvature estimation methods & corresponding unit tests/docs. Working on followup paper characterizing the distribution of the delta hyperbolicity.' },
 	{ role: 'Database/Applied Causal Inference', company: "Wu Lab @ Columbia", period: 'Oct 2024 - May 2025',
 		description: 'Implemented GPU accelerated kNN-based mutual information calculation over relational joins without materialization for SUNA, a scalable causal confounder discovery system.'},
-	{ role: 'Unsupervised Learning Theory/Agentic Data Analysis', company: "Verma Lab @ Columbia", period: 'Jan 2024 - Jan 2025', description: 'Explored dimensionality reduction theoretical bounds. Realized I am not cut out to be a theoretician and began working on agentic data analysis and optimized deployment of unsueprvised learning algorithms in a webapp.' },
+	{ role: 'Unsupervised Learning Theory/Agentic Data Analysis', company: "Verma Lab @ Columbia", period: 'Jan 2024 - Jan 2025', description: 'Explored dimensionality reduction theory. Realized I am not cut out to be a theoretician and began working on agentic data analysis and optimized deployment of unsueprvised learning algorithms in a webapp.' },
 	{ role: 'Prior Fitted Networks applied to EHR data (Specialized transformer architecture for few-shot Bayesian inference)', company: "ReAIM Lab @ Columbia", period: 'May 2024 - Oct 2025', description: 'Modified PFNs to optimize for EHR data (altered synthetic data generation process)' },
 ];
 
